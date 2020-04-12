11dfbb120452ffa758a49e2699f9025955aa67ee
Felix <30987125+flxcn@users.noreply.github.com>
2020-04-11 21:29:31 -0500

    Create head.php

---
+++ head.php
@@ -0,0 +1,72 @@
+<!-- Bootstrap Core CSS -->
+<link href="./css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="./css/half-slider.css" rel="stylesheet">
+<link href="./css/homepagelayout.css" rel="stylesheet">
+
+<!-- homepage css -->
+<link href="./css/homepage.css" rel="stylesheet">
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!--navbar js-->
+<script src="js/toolbar.js"></script>
+
+<style type="text/css">
+
+html
+{
+	height: 100%;
+}
+
+body
+{
+	height: 100%;
+	font: 14px sans-serif;
+	text-align: center;
+	align-items: center;
+	justify-content: center;
+	background-color: #f5f5f5;
+	margin-bottom: 60px;
+}
+
+
+.wrapper{
+	/* centered in page */
+	margin: 0 auto;
+	padding: 20px;
+}
+
+.page-header h2{
+	margin-top: 0;
+}
+
+.col-md-12{
+	overflow: auto;
+}
+
+table
+{
+	/* set table color to white */
+	background-color: #ffffff;
+	text-align: center;
+}
+
+th
+{
+	text-align: center;
+	vertical-align: middle;
+}
+
+td
+{
+	text-align: center;
+	vertical-align: middle;
+}
+
+</style>
